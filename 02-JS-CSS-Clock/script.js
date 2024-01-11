e04c37f2881a66896226f1d0dfe0a898a9b59003
Eileen Xue <xue.eileen@gmail.com>
2024-01-11 01:41:18 -0500

    Challenge 2 - Update clock styles + transition fix

--- 02-JS-CSS-Clock/script.js
+++ 02-JS-CSS-Clock/script.js
@@ -3,27 +3,37 @@ const secondHand = document.querySelector('.second-hand');
 const minuteHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
 
-const currentDate = () => {
 
+// Reset transition animation when the hands reach the full cycle
+const resetTransition = (hand, value) => {
+  if (value === 0) {
+    hand.style.transition = 'none';
+  } else {
+    hand.style.transition = '';
+  }
+}
+
+const currentDate = () => {
   // Get the local current date
   const now = new Date();
 
   // Define seconds
   const seconds = now.getSeconds();
   const secondsToDegrees = ((seconds / 60) * 360) + 90;
+  resetTransition(secondHand, seconds);
   secondHand.style.transform = `rotate(${secondsToDegrees}deg)`;
 
   // Define minutes
   const minutes = now.getMinutes();
   const minutesToDegrees = ((minutes / 60) * 360) + 90;
+  resetTransition(minuteHand, minutes);
   minuteHand.style.transform = `rotate(${minutesToDegrees}deg)`;
 
   // Define hours
   const hours = now.getHours();
   const hoursToDegrees = ((hours / 12) * 360) + 90;
+  resetTransition(hourHand, hours);
   hourHand.style.transform = `rotate(${hoursToDegrees}deg)`;
-
-  console.log(hours, minutes, seconds);
 }
 
-setInterval(currentDate, 1000)
+setInterval(currentDate, 1000);
