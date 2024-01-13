5335c3bc6d00ea494bc51a175aa54bef9dd3c7bd
Eileen Xue <xue.eileen@gmail.com>
2024-01-13 00:53:46 -0500

    Challenge 2 - Prettier up the code

--- 02-JS-CSS-Clock/script.js
+++ 02-JS-CSS-Clock/script.js
@@ -1,17 +1,16 @@
 // Declare variables
-const secondHand = document.querySelector('.second-hand');
-const minuteHand = document.querySelector('.min-hand');
-const hourHand = document.querySelector('.hour-hand');
-
+const secondHand = document.querySelector(".second-hand");
+const minuteHand = document.querySelector(".min-hand");
+const hourHand = document.querySelector(".hour-hand");
 
 // Reset transition animation when the hands reach the full cycle
 const resetTransition = (hand, value) => {
   if (value === 0) {
-    hand.style.transition = 'none';
+    hand.style.transition = "none";
   } else {
-    hand.style.transition = '';
+    hand.style.transition = "";
   }
-}
+};
 
 const currentDate = () => {
   // Get the local current date
@@ -19,21 +18,21 @@ const currentDate = () => {
 
   // Define seconds
   const seconds = now.getSeconds();
-  const secondsToDegrees = ((seconds / 60) * 360) + 90;
+  const secondsToDegrees = (seconds / 60) * 360 + 90;
   resetTransition(secondHand, seconds);
   secondHand.style.transform = `rotate(${secondsToDegrees}deg)`;
 
   // Define minutes
   const minutes = now.getMinutes();
-  const minutesToDegrees = ((minutes / 60) * 360) + 90;
+  const minutesToDegrees = (minutes / 60) * 360 + 90;
   resetTransition(minuteHand, minutes);
   minuteHand.style.transform = `rotate(${minutesToDegrees}deg)`;
 
   // Define hours
   const hours = now.getHours();
-  const hoursToDegrees = ((hours / 12) * 360) + 90;
+  const hoursToDegrees = (hours / 12) * 360 + 90;
   resetTransition(hourHand, hours);
   hourHand.style.transform = `rotate(${hoursToDegrees}deg)`;
-}
+};
 
 setInterval(currentDate, 1000);
